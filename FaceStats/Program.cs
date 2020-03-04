2c5d27914c3f0ce12de429c83da8d31ae95c0d0c
Kristoffer Garmann <garmann@ukult.net>
2020-03-04 13:32:16 +0100

    first commit

---
+++ FaceStats/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace FaceStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long timestamp;
+            int messages=0;
+            bool sender;
+            String path = @"C:\Users\Garmann\Dropbox\FaceStats\";
+            using (StreamWriter sw = File.AppendText(path + "output.json"))
+            {
+                sw.WriteLine("\"messages\": [");
+                for (int i = 1; i < 2; i++)
+                {
+                    using (StreamReader sr = File.OpenText(path + "message_" + i + ".json"))
+                    {
+                        String s = "";
+
+                        while ((s = sr.ReadLine()) != null)
+                        { 
+                            
+                            if (s.Contains("timestamp_ms"))
+                            {
+                                Int64.TryParse(Regex.Match(s, @"\d+").Value, out timestamp);
+                                messages++;
+                                //Console.WriteLine(timestamp);
+                                DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(timestamp);
+                                Console.WriteLine(dateTimeOffset.Date);
+                                
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(messages);
+            }
+            
+        }
+
+    }
+}
